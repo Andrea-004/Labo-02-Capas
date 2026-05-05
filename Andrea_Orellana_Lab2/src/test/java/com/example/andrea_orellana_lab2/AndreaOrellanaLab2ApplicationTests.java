99b29ed6fbfed16c6325efb2e6a4fc9e35a734d1
Andrea-004 <00106123@uca.edu.sv>
2026-05-04 21:18:46 -0600

    feat - adding db and yaml configurations

---
+++ Andrea_Orellana_Lab2/src/test/java/com/example/andrea_orellana_lab2/AndreaOrellanaLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.andrea_orellana_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndreaOrellanaLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
