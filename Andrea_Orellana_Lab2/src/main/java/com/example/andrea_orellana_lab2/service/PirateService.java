b04175f1cfdd12b46382c95ebabfc1a8339a532d
Andrea-004 <00106123@uca.edu.sv>
2026-05-04 22:40:15 -0600

    feat - adding rest of methods, services and controllers

--- Andrea_Orellana_Lab2/src/main/java/com/example/andrea_orellana_lab2/service/PirateService.java
+++ Andrea_Orellana_Lab2/src/main/java/com/example/andrea_orellana_lab2/service/PirateService.java
@@ -2,6 +2,13 @@ package com.example.andrea_orellana_lab2.service;
 
 import com.example.andrea_orellana_lab2.domain.entity.Pirate;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface PirateService {
     void createPirate(Pirate pirate);
+    List<Pirate> getAllPirates();
+    Pirate getPirateById(UUID id);
+    void updatePirate(UUID id, Pirate pirate);
+    Pirate deletePirate(UUID id);
 }
--- Andrea_Orellana_Lab2/src/main/java/com/example/andrea_orellana_lab2/service/impl/PirateServiceImpl.java
+++ Andrea_Orellana_Lab2/src/main/java/com/example/andrea_orellana_lab2/service/impl/PirateServiceImpl.java
@@ -6,6 +6,9 @@ import com.example.andrea_orellana_lab2.service.PirateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class PirateServiceImpl implements PirateService {
@@ -15,4 +18,32 @@ public class PirateServiceImpl implements PirateService {
     public void createPirate(Pirate pirate) {
         pirateRepository.save(pirate);
     }
+
+    @Override
+    public List<Pirate> getAllPirates() {
+        return pirateRepository.findAll();
+    }
+
+    @Override
+    public Pirate getPirateById(UUID id) {
+        return pirateRepository.findById(id).orElseThrow(() -> new RuntimeException("Pirate not found!"));
+    }
+
+    @Override
+    public void updatePirate(UUID id, Pirate pirate) {
+        Pirate exists = pirateRepository.getPirateById(id);
+        exists.setName(pirate.getName());
+        exists.setBounty(pirate.getBounty());
+        exists.setCrew(pirate.getCrew());
+        exists.setIsAlive(pirate.getIsAlive());
+        pirateRepository.save(exists);
+    }
+
+    @Override
+    public Pirate deletePirate(UUID id) {
+        Pirate exist = pirateRepository.getPirateById(id);
+        pirateRepository.deleteById(id);
+        return exist;
+    }
+
 }
